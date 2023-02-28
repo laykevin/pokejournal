fa9e2e4281c85b526b86e04f2dbea73ac1c12fde
Kevin Lay <kevinlay@live.com>
2023-02-28 22:23:31 +0000

    Updated 'submit' callback function to render a DOM tree for new pokemon submitted by using the renderPokemon function. Appends the new DOM tree to the div that holds the sprites.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $pokemonName = document.querySelector('#poke-name');
 var $pokemonNameForm = document.querySelector('#poke-name-form');
+var $spriteBox = document.querySelector('.box-sprites');
 $pokemonNameForm.addEventListener('submit', storePokeData);
 
 function storePokeData(event) {
@@ -18,9 +19,24 @@ function storePokeData(event) {
     pokeData.name = xhr.response.species.name;
     pokeData.entryId = data.nextEntryId;
     data.nextEntryId++;
-    data.entries.unshift(pokeData);
+    data.entries.push(pokeData);
     $pokemonNameForm.reset();
+    $spriteBox.appendChild(renderPokemon(pokeData));
   });
   xhr.send();
 
 }
+
+function renderPokemon(pokemon) {
+  var $sprite = document.createElement('img');
+  $sprite.className = 'sprite';
+  $sprite.src = pokemon.sprites.front_default;
+  $spriteBox.appendChild($sprite);
+  return $sprite;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    renderPokemon(data.entries[i]);
+  }
+});
