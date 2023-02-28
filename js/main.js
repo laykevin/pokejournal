e0cd5db390e2ec7b75844952513c190c4ba6d436
Kevin Lay <kevinlay@live.com>
2023-02-28 18:30:39 +0000

    Added error message for Pokemon that don't exist. Made sure to only update the data model for valid Pokemon.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-var $addPokemon = document.querySelector('.add-button');
 var $pokemonName = document.querySelector('#poke-name');
 var $pokemonNameForm = document.querySelector('#poke-name-form');
-$addPokemon.addEventListener('click', storePokeData);
-var pokeData = {};
+$pokemonNameForm.addEventListener('submit', storePokeData);
+
 function storePokeData(event) {
   event.preventDefault();
-  getPokeData($pokemonName.value);
-  pokeData.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(pokeData);
-  $pokemonNameForm.reset();
-}
-
-function getPokeData(name) {
+  var pokeData = {};
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + $pokemonName.value.toLowerCase());
   xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
+  xhr.addEventListener('load', function (event) {
+    if (xhr.status !== 200) {
+      $pokemonName.setCustomValidity("That Pokemon doesn't exist!");
+      $pokemonName.reportValidity();
+      return;
+    }
     pokeData.sprites = xhr.response.sprites;
     pokeData.name = xhr.response.species.name;
+    pokeData.entryId = data.nextEntryId;
+    data.nextEntryId++;
+    data.entries.unshift(pokeData);
+    $pokemonNameForm.reset();
   });
   xhr.send();
+
 }
