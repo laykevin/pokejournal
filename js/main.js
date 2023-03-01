4d31f6341a3dcbcc334748493f37892c23558bfe
Kevin Lay <kevinlay@live.com>
2023-03-01 21:53:09 +0000

    Pre populates modal with stored data of pokemon that was clicked.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var $pokemonName = document.querySelector('#poke-name');
 var $pokemonNameForm = document.querySelector('#poke-name-form');
 var $spriteBox = document.querySelector('.box-sprites');
+var $modal = document.querySelector('#modal');
+var $officialArt = document.querySelector('.official-art');
+var $editingName = document.querySelector('.poke-name');
+var $xButton = document.querySelector('.fa-circle-xmark');
 $pokemonNameForm.addEventListener('submit', storePokeData);
 
 function storePokeData(event) {
   event.preventDefault();
-  var pokeData = {};
+  var pokeData = {
+    nickname: '',
+    moves: ''
+  };
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + $pokemonName.value.toLowerCase());
   xhr.responseType = 'json';
@@ -30,6 +37,7 @@ function storePokeData(event) {
 function renderPokemon(pokemon) {
   var $sprite = document.createElement('img');
   $sprite.className = 'sprite';
+  $sprite.setAttribute('data-entry-id', pokemon.entryId);
   $sprite.src = pokemon.sprites.front_default;
   $spriteBox.appendChild($sprite);
   return $sprite;
@@ -39,4 +47,40 @@ document.addEventListener('DOMContentLoaded', function (event) {
   for (var i = 0; i < data.entries.length; i++) {
     renderPokemon(data.entries[i]);
   }
+});
+
+var $editingForm = document.querySelector('#editing-form');
+var $nickName = document.querySelector('#nickname');
+var $nature = document.querySelector('#nature');
+var $gender = document.querySelector('#gender');
+var $moves = document.querySelector('#moves');
+$spriteBox.addEventListener('click', showModal);
+function showModal(event) {
+  if (event.target.className === 'sprite') {
+    $modal.className = 'modal';
+    for (var i = 0; i < data.entries.length; i++) {
+      if (event.target.getAttribute('data-entry-id') === data.entries[i].entryId.toString()) {
+        data.editing = data.entries[i];
+        $officialArt.src = data.entries[i].sprites.other['official-artwork'].front_default;
+        $editingName.textContent = data.entries[i].name.charAt(0).toUpperCase() + data.entries[i].name.slice(1);
+        $nickName.value = data.editing.nickname;
+        $nature.value = data.editing.nature;
+        $gender.value = data.editing.gender;
+        $moves.value = data.editing.moves;
+      }
+    }
+  }
+}
+
+$editingForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  data.editing.nickname = $nickName.value;
+  data.editing.nature = $nature.value;
+  data.editing.gender = $gender.value;
+  data.editing.moves = $moves.value;
+  $modal.className = 'modal hidden';
+});
+
+$xButton.addEventListener('click', function (event) {
+  $modal.className = 'modal hidden';
 });
